9dba06784d559c1d5f0b400ee6960087ccdcbd02
Alex Andrew <pytormal@live.com>
2020-07-14 17:12:37 -0700

    all task completed

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ Airplane.prototype.land = function () {
   this.isFlying = false;
 };
 
-
 /*
 // 👇 COMPLETE YOUR WORK BELOW 👇
 // 👇 COMPLETE YOUR WORK BELOW 👇
@@ -29,39 +28,65 @@ Airplane.prototype.land = function () {
 /*
   TASK 1
     - Write a Person Constructor that initializes `name` and `age` from arguments.
+
     - All instances of Person should initialize with an empty `stomach` array.
+
     - Give instances of Person the ability to `.eat("someFood")`:
         + When eating an edible, it should be pushed into the `stomach`.
         + The `eat` method should have no effect if there are 10 items in the `stomach`.
+
     - Give instances of Person the ability to `.poop()`:
         + When an instance poops, its `stomach` should empty.
+
     - Give instances of Person a method `.toString()`:
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person(name, age) {
-  this.name = name;
-  this.age = age;
+// function Person(name, age) {
+//   this.name = name;
+//     this.age = age;
+//   this.stomach = [];
+// }
+// Person.prototype.eat = function () {
+//   this.stomach = [];
+// };
+// Person.prototype.poop = function () {
+//   this.stomach = [];
+// };
+
+function Person(attrs) {
+  this.name = attrs.name;
+  this.age = attrs.age;
   this.stomach = [];
+  // this.tostring = function (name, age) {
+  //   console.log(`${this.name} ${this.age}`);
+  // };
 }
 Person.prototype.eat = function () {
   this.stomach = [];
+  console.log(`${this.name} is eating`);
 };
 Person.prototype.poop = function () {
-  this.stomach = [];
+  console.log(`${this.name} says ${this.stomach}`);
 };
-
+console.log(" - - - Task 1");
+let personInfo = new Person({ name: "Kyle", age: 50 });
+console.log(personInfo.name, personInfo.age);
 
 /*
   TASK 2
     - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
+
     - All instances built with Car:
         + should initialize with an `tank` at 0
         + should initialize with an `odometer` at 0
+
     - Give cars the ability to get fueled with a `.fill(gallons)` method. Add the gallons to `tank`.
+
     - STRETCH: Give cars ability to `.drive(distance)`. The distance driven:
         + Should cause the `odometer` to go up.
         + Should cause the the `tank` to go down taking `milesPerGallon` into account.
+
     - STRETCH: A car which runs out of `fuel` while driving can't drive any more distance:
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
@@ -71,17 +96,19 @@ function Car(model, milesPerGallon) {
   this.milesPerGallon = milesPerGallon;
   this.tank = 0;
   this.odometer = 0;
-  this.stomach = [];
 }
 Car.prototype.fill = function (gallons) {
   ++this.tank;
-
+  console.log(`${this.tank}`);
 };
 Car.prototype.drive = function (distance) {
   ++this.odometer;
   --this.tank;
+  console.log(this.odometer);
+  console.log(this.tank);
 };
 
+console.log(" - - - Task 2");
 
 /*
   TASK 3
@@ -90,28 +117,50 @@ Car.prototype.drive = function (distance) {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
 
+function Baby(attrs) {
+  Person.call(this, attrs);
+  this.favoriteToy ="pizza"
+  };
+Baby.prototype.play = function () {
+console.log(`Playing with ${this.favoriteToy}.`)
 }
+let toString = new Baby(Baby)
+console.log(" - - - Task 3");
+toString.play();
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+
+    1. Implicit Binding: left of the Dot at Call time. (this.name) in other words be (me.name)
+  
+    2. Explicit Binding: call, apply
+    The function is one the global scope, would need to use .call or.apply to invoke the global function. 
+    
+    each one is used differently
+    
+    3. new Binding: "this is refered to a or the specific instance of that particular object that is created and returnd by the function that is the constructor"
+  
+    4. window Binding: "this" uses the global scope
 */
 
-
 ///////// END OF CHALLENGE /////////
 ///////// END OF CHALLENGE /////////
 ///////// END OF CHALLENGE /////////
-if (typeof exports !== 'undefined') {
-  module.exports = module.exports || {}
-  if (Airplane) { module.exports.Airplane = Airplane }
-  if (Person) { module.exports.Person = Person }
-  if (Car) { module.exports.Car = Car }
-  if (Baby) { module.exports.Baby = Baby }
+if (typeof exports !== "undefined") {
+  module.exports = module.exports || {};
+  if (Airplane) {
+    module.exports.Airplane = Airplane;
+  }
+  if (Person) {
+    module.exports.Person = Person;
+  }
+  if (Car) {
+    module.exports.Car = Car;
+  }
+  if (Baby) {
+    module.exports.Baby = Baby;
+  }
 }
